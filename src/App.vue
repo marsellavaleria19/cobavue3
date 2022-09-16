4a6cc82afc9f0065fe7e58fbe9b524c7e257927a
marsellavaleria19 <marsellavaleria19@gmail.com>
2022-09-16 09:09:53 +0700

    feat : init

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1 class="mb-4 text-3xl font-bold text-red-400 underline">
+      Tailwind CSS 3 with vue js 3
+    </h1>
+    <button class="px-10 py-5 text-sm text-white bg-green-600 rounded">
+      Next
+    </button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
 
 <style>
